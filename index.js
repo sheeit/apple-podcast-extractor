de6d34b0041cf56882dd43deda2368057d0f1a92
strupo <sheeit@users.noreply.github.com>
2021-08-20 21:22:00 +0000

    Add files
    Could be improved but I think it's pretty good and doesn't have any dependencies (none of that Bootstrap or jQuery crap).

---
+++ index.js
@@ -0,0 +1,227 @@
+"use strict";
+
+var callbackfunc = (function () {
+
+    var
+        applePodcastsBaseURL = "https://podcasts.apple.com/podcast/id",
+        hiddenTtiles,
+        lastinput,
+        navdiv,
+        resultcounter = 0,
+        resultsdiv;
+
+    function getrss(formdata) {
+        var
+            action = formdata.get("action"),
+            script = document.getElementById("injected-script"),
+            podIdRegex = /(?:https?:\/\/podcasts.apple.com\/.*\/id)?(\d+)$/,
+            url = formdata.get("input");
+
+        lastinput = action + " " + url;
+
+        if (action === "lookup") {
+            url = podIdRegex.exec(url);
+
+            if (url === null) {
+                alert("No Podcast ID found");
+                return false;
+            }
+
+            url = url[1];
+
+            var apiurl = "https://itunes.apple.com/lookup"
+                + "?id=" + encodeURIComponent(url)
+                + "&entity=podcast"
+                + "&callback=callbackfunc";
+
+        } else if (action === "search") {
+            var apiurl = "https://itunes.apple.com/search"
+                + "?term=" + encodeURIComponent(url).replaceAll("%20", "+")
+                + "&country=US"
+
+        } else {
+            alert("Unknown action: " + action);
+            return false;
+        }
+
+        apiurl += "&entity=podcast&callback=" + callbackfunc.name;
+
+        // Remove the old script
+        if (script)
+            script.remove();
+
+        script = document.createElement("script");
+        script.src = apiurl;
+        script.id = "injected-script";
+        document.body.append(script);
+        return true;
+    }
+
+    function load(ignored) {
+        hiddenTtiles = Array.from(document.getElementsByTagName("h2"));
+        navdiv = document.getElementById("nav");
+        resultsdiv = document.getElementById("results");
+
+        document.forms[0].addEventListener("submit", function (e) {
+            e.preventDefault();
+            var formdata = new FormData(e.target);
+            getrss(formdata);
+        });
+    }
+
+    function getNavUl() {
+        var navul = document.getElementById("navul");
+        if (!navul) {
+            navul = document.createElement("ol");
+            navul.id = "navul";
+            navdiv.appendChild(navul);
+        }
+        return navul;
+    }
+
+    function selectOnClick(event) {
+        var
+            selection = window.getSelection(),
+            range = document.createRange();
+
+        range.selectNode(event.target);
+        selection.addRange(range);
+
+        event.stopPropagation();
+        event.preventDefault();
+    }
+
+    function imageSrcToggler(one, two) {
+        function clickHandler(event) {
+            var src = event.target.src;
+            event.target.src = src === one ? two : one;
+        }
+        return clickHandler;
+    }
+
+    function displayResult(result) {
+        var
+            fragment = document.createDocumentFragment(),
+            li = document.createElement("li"),
+            image = document.createElement("img"),
+            nameh3 = document.createElement("h3"),
+            artistp = document.createElement("p"),
+            artista = document.createElement("a"),
+            feedp = document.createElement("p"),
+            feedspan = document.createElement("span"),
+            podcastidp = document.createElement("p"),
+            podcastida = document.createElement("a"),
+            genres = document.createElement("ul"),
+            clearFix = document.createElement("div");
+
+        image.src = result.artworkUrl100;
+        image.addEventListener("click",
+            imageSrcToggler(result.artworkUrl100, result.artworkUrl600));
+
+        nameh3.appendChild(document.createTextNode(result.trackName));
+        nameh3.appendChild(document.createElement("br"));
+
+        artistp.className = "podcast-artist";
+        artista.appendChild(document.createTextNode(result.artistName));
+        artista.href = result.artistViewUrl;
+        artistp.appendChild(artista);
+
+        feedspan.className = "podcast-feedurl";
+        feedspan.appendChild(document.createTextNode(result.feedUrl));
+        feedspan.addEventListener("click", selectOnClick);
+        feedp.appendChild(document.createTextNode("Feed URL: "));
+        feedp.appendChild(feedspan);
+
+        podcastidp.className = "podcast-id";
+        podcastidp.appendChild(document.createTextNode(
+            "Podcast ID: "
+        ));
+        podcastida.appendChild(document.createTextNode(
+            "id" + result.trackId
+        ));
+        podcastida.href = result.trackViewURL;
+        podcastidp.appendChild(podcastida);
+
+        genres.className = "genres";
+        result.genres.forEach(function (genre) {
+            var li = document.createElement("li");
+            if (genre === result.primaryGenreName) {
+                var b = document.createElement("b");
+                b.textContent = genre;
+                li.appendChild(b);
+            } else {
+                li.textContent = genre;
+            }
+            genres.appendChild(li);
+        });
+
+        clearFix.className = "clearfix";
+
+        li.className = "podcast-li";
+        li.appendChild(image);
+        li.appendChild(nameh3);
+        li.appendChild(artistp);
+        li.appendChild(feedp)
+        li.appendChild(podcastidp);
+        li.appendChild(genres);
+        li.appendChild(clearFix);
+
+        fragment.appendChild(li);
+
+        return fragment;
+    }
+
+    function callbackfunc(data) {
+        console.log(data);
+
+        var
+            navul = getNavUl(),
+            navli = document.createElement("li"),
+            navp = document.createElement("p"),
+            nava = document.createElement("a"),
+            ul = document.createElement("ul"),
+            uldiv = document.createElement("div"),
+
+            gotonavp = document.createElement("p"),
+            gotonava = document.createElement("a"),
+
+            resultFragments = data.results.map(displayResult);
+        
+        hiddenTtiles.forEach(function (h2) {
+            h2.className = "";
+        });
+
+        nava.href = "#result-" + resultcounter;
+        nava.textContent = lastinput + " (" + data.resultCount + " results)";
+        navp.appendChild(nava);
+        navli.appendChild(navp);
+        navul.appendChild(navli);
+
+        uldiv.id = "result-" + resultcounter++;
+
+        // If the results div only has one child element, then it's the <h2>
+        // and it should be ignored.
+        if (resultsdiv.childElementCount > 1) {
+            resultsdiv.insertBefore(uldiv, resultsdiv.children[1]);
+        } else {
+            resultsdiv.appendChild(uldiv);
+        }
+
+        resultFragments.forEach(function (fragment) {
+            ul.appendChild(fragment);
+        });
+
+        gotonava.href = "#top";
+        gotonava.innerHTML = "&uarr; Go top &uarr;";
+        gotonavp.appendChild(gotonava);
+        uldiv.appendChild(ul);
+        uldiv.appendChild(gotonavp);
+        uldiv.appendChild(document.createElement("hr"));
+    };
+
+
+    window.addEventListener("load", load);
+
+
+    return callbackfunc;
+})();
